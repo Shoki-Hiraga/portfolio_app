5c535766141c7a0231ed468f380650c77cfe2ed5
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-03-27 23:11:58 +0900

    プロフィール追加

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -9,28 +9,47 @@
 </head>
 <body>
     <h1>{{$python_h1}}</h1>
-    <h2>職務経歴</h2>
     <div class="history">
-        <h3 class="history">
-            <a href="{{ route('mp_paltek') }}">株式会社PALTEK
-                <span class="top_year">@include('components.year', ['target' => 'paltek'])</sapn>
-            </a>
-        </h3>
-        <h3 class="history">
-            <a href="{{ route('mp_yourmystar') }}">ユアマイスター株式会社
-                <span class="top_year">@include('components.year', ['target' => 'yourmystar'])</sapn>
-            </a>
-        </h3>
-        <h3 class="history">
-            <a href="{{ route('mp_idealump') }}">株式会社アイデアランプ
-                <span class="top_year">@include('components.year', ['target' => 'idealump'])</sapn>
-            </a>
-        </h3>
-        <h3 class="history">
-            <a href="{{ route('mp_current') }}">カレント自動車株式会社
-                <span class="top_year">@include('components.year', ['target' => 'current'])></sapn>
-        </h3>
+        <h2>自己紹介</h2>
+            <p>1992年11月24日、神奈川県横浜市で生まれ、中学、高校ではバレーボール部で副主将、主将を務める。<br>
+                大学では念願の心理学を専攻して没頭。<br>
+                その傍らで車とバイクの整備に没頭。
+            </p>
+            <p>車とバイクとDIY好きを拗らせ、日産スカイラインに、日産ローレルのフロントフェイスを組合せる<br>"顔面スワップ"をプライベートで成し遂げる</p>
+            <img src="{{ asset('images/samukawa.jpg') }}" alt="車の画像" class="top_image">
 
-</div>
+            <p>その後、WebディレクターになるきっかけとなったWPのブログサイト、<a href="https://332blog.com/">332blog</a> を立ち上げる
+            </p>
+            <p>
+                Web制作会社にて、ディレクターとして <a href="https://shisaly.com/">shisaly</a> のシステム開発ディレクターとして従事。<br>
+                Webサイトを作る楽しさを知り、ディレクターとしてchatGPTを駆使して、Webサイトを立ち上げ。<br>
+                <a href="https://carprice-info.332web.com/">中古車買取相場データベース</a>
+                <p>Laravel フレームワークを使い、pythonにてスクレイピング処理をcronにて実行。<br>
+                    スクレイピングしたデータは直接DBに格納し、多少データを加工してLaravelと共有しているマスターデータに相場情報を統合する。
+                </p>
+                <img src="{{ asset('images/market_price.png') }}" alt="Webサイト構成図" class="top_image">
+
+            </p>
+        <h2>職務経歴一覧</h2>
+            <h3 class="history">
+                <a href="{{ route('mp_paltek') }}">株式会社PALTEK
+                    <span class="top_year">@include('components.year', ['target' => 'paltek'])</sapn>
+                </a>
+            </h3>
+            <h3 class="history">
+                <a href="{{ route('mp_yourmystar') }}">ユアマイスター株式会社
+                    <span class="top_year">@include('components.year', ['target' => 'yourmystar'])</sapn>
+                </a>
+            </h3>
+            <h3 class="history">
+                <a href="{{ route('mp_idealump') }}">株式会社アイデアランプ
+                    <span class="top_year">@include('components.year', ['target' => 'idealump'])</sapn>
+                </a>
+            </h3>
+            <h3 class="history">
+                <a href="{{ route('mp_current') }}">カレント自動車株式会社
+                    <span class="top_year">@include('components.year', ['target' => 'current'])></sapn>
+            </h3>
+    </div>
 </body>
 </html>
--- resources/views/main/index_idealump.blade.php
+++ resources/views/main/index_idealump.blade.php
@@ -6,66 +6,71 @@
 @include('components.link')
 
 <title>
-ユアマイスター株式会社 @include('components.year', ['target' => 'yourmystar'])
+株式会社アイデアランプ @include('components.year', ['target' => 'idealump'])
 @include('components.sitename')
 </title>
 </head>
 <body>
-    <h1>ユアマイスター株式会社<br>
-      <span class="start_end">@include('components.year', ['target' => 'yourmystar'])</span>
+    <h1>株式会社アイデアランプ<br>
+      <span class="start_end">@include('components.year', ['target' => 'idealump'])</span>
     </h1>
-    <h2></h2>
+    <h2>Webサイト・アプリ制作／Webマーケティング支援</h2>
 
 <div class="content">
   <h3>仕事内容</h3>
-  <p>ユアマイスターの電話営業</p>
+  <p>
+    Webサイト制作におけるディレクション業務と、Google Analyticsなどを用いたWebマーケティング業務を兼務。
+  </p>
 
   <h3>営業スタイル</h3>
-  <p>新規開拓 100％ 電話営業 (1日100件)</p>
+  <p>ディレクション業務・データ分析を元にした改善提案型マーケティング</p>
 
   <h3>クライアント</h3>
-  <p>個人事業主 (法人含む) ハウスクリーニング事業者</p>
+  <p>企業サイト運営会社、EC事業者など</p>
 
   <h3>実績</h3>
   <ul>
     <li>
-      <strong>5月から営業チームへ本格配属。</strong><br>
-      コロナ禍により、自宅での電話営業のみ。チーム全体で中盤をキープ。<br>
-      5月～9月の5か月で<strong>40件の顧客を獲得</strong>
-    </li>
-    <li>
-      <strong>7月以降はメルマガ担当を兼務</strong><br>
-      (5～6月のメール配信開封率、案件獲得率は営業部No.1)<br>
-      作業的にやっていたメルマガに図説を加えて好評に。
+      <strong>Webディレクション業務</strong><br>
+      新規Webサイト制作および既存サイトの機能追加における要件定義・ワイヤー作成・画面設計・進行管理などを担当。
       <ul>
-        <li>メルマガ平均配信数：1,650件</li>
-        <li>開封率：31.5%（530件）※平賀担当前は平均25%前後</li>
-        <li>クリック率：0.7%（12件）</li>
-        <li>案件獲得率：0.5%（9件）※メール開封数をベースに算出</li>
+        <li><a href="https://shisaly.com/" target="_blank">shisaly.com</a>：Laravelを用いたフルスクラッチ案件（予算1,500万円）</li>
+        <li><a href="https://rabostar.com/" target="_blank">rabostar.com</a>：ASP・PHPによるスクラッチ開発（予算1,000万円）</li>
+        <li>UI/UXを考慮した機能要件の仕様確定・デザイン確定・DBロジック連携</li>
       </ul>
     </li>
+
     <li>
-      <strong>その他の指標</strong>
+      <strong>Webマーケティング業務</strong><br>
+      Google AnalyticsやSEOツールを用いた競合調査・改善提案・広告運用まで幅広く対応。
       <ul>
-        <li>契約後のクレーム件数：半年で1件</li>
-        <li>契約の途中離脱率：<strong>チームでNo.1（0.27%）</strong>（平均は約10%）</li>
-        <li>出店後の顧客の総売上ランキング：6人中3位</li>
+        <li>使用ツール：Google Analytics / Search Console / ahrefs / ubersuggest</li>
+        <li>レポートや改善提案書の作成 → リニューアルディレクションにつなげる</li>
+        <li>広告媒体：Google・Yahoo!リスティング／ディスプレイ広告</li>
+        <li>ECサイト広告にてROAS 300%を維持</li>
+      </ul>
+    </li>
+
+    <li>
+      <strong>その他の実績</strong>
+      <ul>
+        <li>2021年12月、Google広告にてCVR 1.7%改善（前年同月比）</li>
+        <li>社内標準化：テスト修正依頼用シートを独自作成・チーム内で導入</li>
+        <li>自社メディア改善：SEO構造ガイド作成 → 3ヶ月で15%アクセス増（YoY）</li>
+        <li><a href="https://drive.google.com/drive/folders/1zI9YlOzoc-_4rG8gsx6Zcoylb5I8c4lq?usp=sharing" target="_blank">マーケ資料ポートフォリオ</a></li>
       </ul>
     </li>
   </ul>
 
   <h3>ポイント</h3>
   <p>
-    無料プランに出店されている業者様を有料プランへのアップセルプロジェクトを担当。
+    ディレクターとして制作工程を一気通貫で担当しつつ、Webマーケターとして分析・改善も主導。
     <br>
-    1か月間限定にて、無料プランでも簡易的な有料プランを体験していただくことで、売上UP ⇒ 有料プランへ導入を目的としたプロジェクト。
+    開発チーム・デザインチーム・マーケチームとの密な連携によって、戦略から実装、運用までを網羅したディレクションを実現。
     <br>
-    大きな数字的結果は出せなかったが、<strong>商品ページの作成フォーマット</strong>をチームメンバーと制作。
-    <br>
-    営業部隊の業務効率化に貢献。このフォーマットはプロジェクト終了の翌月から<strong>新入社員研修の一環</strong>として活用されている。
+    データ分析に基づいた提案型の広告運用やSEO施策が強み。
   </p>
 </div>
-
 
 </body>
 </html>
