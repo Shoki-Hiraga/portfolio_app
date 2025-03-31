8f9a95099cd14b6dde5c5e3bea5fd6c9f8f6995b
shoki-hiraga <chaser.cb750@gmail.com>
2025-03-31 17:40:07 +0900

    GTM

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,3 +4,16 @@
 <link rel="stylesheet" href="{{ asset('/css/profile_card.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/job_history.css') }}">
 @include('components.breadcrumb')
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3CKGSVB');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3CKGSVB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
