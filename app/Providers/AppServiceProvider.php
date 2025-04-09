1dcb195ce958678676f1fc11a7a0534d404b08d5
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-04-09 20:12:01 +0900

    年間のミス修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $companies_year = [
             'paltek' => ['start' => '2015/03', 'end' => '2020/03'],
-            'yourmystar' => ['start' => '2020/04', 'end' => '2021/03'],
+            'yourmystar' => ['start' => '2020/04', 'end' => '2020/12'],
             'idealump' => ['start' => '2021/04', 'end' => '2023/03'],
             'current' => ['start' => '2023/04', 'end' => '現在']
         ];
